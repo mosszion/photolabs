e19bf275dec7a8b20eafa4dcc050d43b6ca3132a
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 14:49:10 -0400

    add span elements for badge

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
+import '../styles/FavBadge.scss'
+import FavBadge from './FavBadge';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo">PhotoLabs</span> 
+      <span className='fav-badge'><span className='fav-badge__count'></span><FavBadge/></span>
     </div>
   )
 }
