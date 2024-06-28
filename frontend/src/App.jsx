0def39a919eaa9474ae628a0df635904907b87ec
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 15:17:53 -0400

    remove unused import lines

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import FavIcon from 'components/FavIcon';
-import PhotoFavButton from 'components/PhotoFavButton';
+
+
 import PhotoList from 'components/PhotoList';
-import TopicListItem from 'components/TopicListItem';
-import TopicList from 'components/TopicList';
+
+
 import TopNavigation from 'components/TopNavigationBar';
  
 
 
 
 const App = () => {
-  // const photoInfos = new Array(3).fill(photoInfo);
-
-
-  // const multiPhotoInfos = photoInfos.map((photoInfo,index) => {
-  //   return (
-  //           <PhotoListItem key = {index} photoInfo={photoInfo} 
-  //           />
-           
-          
+     
 
   // )})
   return (
     <div className="App">
-      {/* {multiPhotoInfos} */}
+ 
       <TopNavigation />
     
       <PhotoList />
