1724d948e71408abcec316316d7462058a85ddf9
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 13:27:16 -0400

    remove some of previous codes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,34 +4,27 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import FavIcon from 'components/FavIcon';
 import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
 
-const photoInfo = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};// Note: Rendering a single component to build components in isolation
+
 
 
 const App = () => {
-  const photoInfos = new Array(3).fill(photoInfo);
+  // const photoInfos = new Array(3).fill(photoInfo);
 
 
-  const multiPhotoInfos = photoInfos.map((photoInfo,index) => {
-    return (
-            <PhotoListItem key = {index} photoInfo={photoInfo} 
-            />
+  // const multiPhotoInfos = photoInfos.map((photoInfo,index) => {
+  //   return (
+  //           <PhotoListItem key = {index} photoInfo={photoInfo} 
+  //           />
            
           
 
-  )})
+  // )})
   return (
     <div className="App">
-      {multiPhotoInfos}
+      {/* {multiPhotoInfos} */}
+      <PhotoList />
     </div>
   );
 };
