a80ab460110397c791e8f7d963a58ed44df3c6aa
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 07:35:09 -0400

    add class names and isolate elements using divs

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -11,10 +11,19 @@ const PhotoListItem = ({photoInfo}) => {
   return (
     <div className="photo-list__item">
 
-    <img src={photoInfo.imageSource}/>
-    <img src={photoInfo.profile}/>
+    <img className="photo-list__image" src={photoInfo.imageSource}/>
+    <div className="photo-list__user-details">
+
+    <img className="photo-list__user-profile" src={photoInfo.profile}/>
+    <div className="photo-list__user-info">
     <p>{photoInfo.username}</p>
+    <div className="photo-list__user-location">
+
     <p>{photoInfo.location.city} {photoInfo.location.country}</p>
+    </div>
+
+    </div>
+    </div>
 
     </div> 
   )
