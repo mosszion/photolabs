652cdebf28fbcb67baad6c33f318e3a7b8eac0e9
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 12:36:59 -0400

    rename function name and add props

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,16 +3,16 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
   const [selected, setSelected] = useState(false);
-  const displayAlert = () => {
+  const toggleSelected = () => {
     setSelected(prev => ! prev)
   }
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
-        <FavIcon onClick = {displayAlert} selected = {selected}/>
+        <FavIcon onClick = {toggleSelected} selected = {selected}/>
        
       </div>
     </div>
