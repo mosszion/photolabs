2630da060a60379dc829e7e224b7eddfeccb2ec2
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 12:29:14 -0400

    add fav icon state

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,13 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton({FavIcon}) {
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
+        {FavIcon}
+       
       </div>
     </div>
   );
