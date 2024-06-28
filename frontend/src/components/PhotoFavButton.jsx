ffc4e53f818196fb38ff494df0c97e88a567ec60
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 12:31:40 -0400

    add state and event to the compoent

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,12 +3,16 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton({FavIcon}) {
+function PhotoFavButton() {
+  const [selected, setSelected] = useState(false);
+  const displayAlert = () => {
+    setSelected(prev => ! prev)
+  }
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
-        {FavIcon}
+        <FavIcon onClick = {displayAlert} selected = {selected}/>
        
       </div>
     </div>
