657308c9b695bed77eb95b1c7701a9e545c08ca1
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-28 15:03:17 -0400

    add topic list

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import '../styles/TopNavigationBar.scss'
 import '../styles/FavBadge.scss'
 import FavBadge from './FavBadge';
+import TopicList from './TopicList';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span> 
-      <span className='fav-badge'><span className='fav-badge__count'></span><FavBadge/></span>
+      <TopicList />
+      <span className='fav-badge'><span className='fav-badge__count'></span><FavBadge isFavPhotoExist={isFavPhotoExist ? true :}/></span>
     </div>
   )
 }
