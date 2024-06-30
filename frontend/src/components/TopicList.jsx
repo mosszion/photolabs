d0c84b1628de0628db37d218978f830dc9206565
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-06-30 06:23:58 -0400

    add topics from data file

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,28 +2,29 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
 
-const dataTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const dataTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
 
-  const multiTopicList = dataTopicList.map((topic) => {
+  const multiTopicList = topics.map((topic) => {
     return (
       < TopicListItem key = {topic.id} topic = {topic} />
     )
