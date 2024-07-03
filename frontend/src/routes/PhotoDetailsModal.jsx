bb3e0fb4606a73c2efb301995d2724481fbc410f
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-07-03 16:39:46 -0400

    add logic to display photo in modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,11 +4,21 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
+  console.log(props)
+  const ph = props.photos.map((photo) => {
+    return <div key = {photo.id} >
+             <img src={photo.urls.regular}/>
+             </div>
+  })
+  console.log(ph)
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick= {() => {props.setDisplayModal(false)}}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      {ph}
+     
+      
     </div>
   )
 };
