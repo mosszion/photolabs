ae8e142c96740eaba107336763098f390deb489a
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-07-03 21:28:55 -0400

    format code

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,40 +3,41 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-
-
 const PhotoListItem = (props) => {
-
-
   /* Insert React */
   return (
-    <div className="photo-list__item" key= {props.photoInfo.id}>
-      <PhotoFavButton  
-      photoInfoId = {props.photoInfo.id}
-      favourites={props.favourites} 
-      toggleFavourite={props.toggleFavourite} 
-       />
-      
+    <div className="photo-list__item" key={props.photoInfo.id}>
+      <PhotoFavButton
+        photoInfoId={props.photoInfo.id}
+        favourites={props.favourites}
+        toggleFavourite={props.toggleFavourite}
+      />
 
-    <img className="photo-list__image" src={props.photoInfo.urls.regular} onClick = {() => {
-      props.setDisplayModal(true);
-      props.setDetail(props.photoInfo)  
-    }}/>
-    <div className="photo-list__user-details">
-
-    <img className="photo-list__user-profile" src={props.photoInfo.user.profile}/>
-    <div className="photo-list__user-info">
-    <p>{props.photoInfo.user.username}</p>
-    <div className="photo-list__user-location">
-
-    <p>{props.photoInfo.location.city}, {props.photoInfo.location.country}</p>
+      <img
+        className="photo-list__image"
+        src={props.photoInfo.urls.regular}
+        onClick={() => {
+          props.setDisplayModal(true);
+          props.setDetail(props.photoInfo);
+        }}
+      />
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={props.photoInfo.user.profile}
+        />
+        <div className="photo-list__user-info">
+          <p>{props.photoInfo.user.username}</p>
+          <div className="photo-list__user-location">
+            <p>
+              {props.photoInfo.location.city},{" "}
+              {props.photoInfo.location.country}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-
-    </div>
-    </div>
-
-    </div> 
-  )
+  );
 };
 
 export default PhotoListItem;
