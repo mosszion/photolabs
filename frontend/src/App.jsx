2d759fbedcd0d92537f3ce588a63341fb2b9387c
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-07-03 20:29:36 -0400

    add detail state to PhotoDetailsModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,6 +34,7 @@ const App = () => {
       {displayModal && <PhotoDetailsModal 
           setDisplayModal={setDisplayModal}
           setDetail = {setDetail}
+          detail = {detail}
           
            
       
