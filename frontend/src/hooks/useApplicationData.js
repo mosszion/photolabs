597e2b6410002a4339f25650b763cabfbcd35797
Moss2 <moss@Mosis-MacBook-Pro.local>
2024-07-08 12:06:44 -0400

    add useReducer logic

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,11 +1,11 @@
-import React,{useState} from "react";
+import React,{useState,useReducer} from "react";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
 
 
 const useApplicationData = () => {
 
     //state object containing the entire state of the application
-    const [state, setState] = useState (
+    const initialState = 
         { 
             displayModal: false,
             detail: null,
@@ -13,57 +13,93 @@ const useApplicationData = () => {
 
 
             
-        }
-    )
+            }
+  const reducer = (state, action) => {
+
+      switch (action.type) {
+        case 'FAV_PHOTO_ADDED': 
+      
+            if (!state.favourites.includes(action.payload)) {
+              return {
+                ...state,
+                favourites: [...state.favourites, action.payload],
+              };
+            }
+            return state;
+
+
+        case 'FAV_PHOTO_REMOVED':
+              return {
+                ...state,
+                favourites: state.favourites.filter((photoId) => photoId !== action.payload),
+              };
+
+        case 'onOpen' : 
+              return {
+                ...state,displayModal: true
+              }
+      
+        case 'onClose' : 
+              return {
+                ...state,displayModal: false
+              }
+        case 'onLoad' : 
+              return {
+                ...state,
+                detail: action.payload
+
+              }
+      
+        default:
+              return state;
+
+            }
+   }
+      
+        
+    const [state, dispatch] = useReducer(reducer, initialState);
   
-
-    const toggleFavourite = (id) => {
-        setState((prevState) => ({
-          ...prevState,
-          favourites: prevState.favourites.includes(id)
-            ? prevState.favourites.filter((photoId) => photoId !== id)
-            : [...prevState.favourites, id],
-        }));
-      };
-
-    
-    
-    const onClosePhotoDetailsModal = () => {
-        setState((prevState) => ({
-          ...prevState,
-          displayModal: false,
-        }));
-      };
-
-      const onOpenPhotoDetailsModal = () => {
-        setState((prevState) => ({
-          ...prevState,
-          displayModal: true,
-        }));
-      };
-   
-      const onLoadDetail = (photoInfo) =>{
-        setState( (prevState) => (
-         {
-            ...prevState,
-             detail:photoInfo
-        }))
-
+      const FAV_PHOTO_ADDED = (id) => {
+        dispatch ( {type : 'FAV_PHOTO_ADDED', payload : id});
       }
+      const FAV_PHOTO_REMOVED = (id) => {
+        dispatch ( {type : 'FAV_PHOTO_REMOVED', payload : id});
+        }
+      const onOpenPhotoDetailsModal = () => {
+        dispatch ({type: 'onOpen'});
+      }
+      const onClosePhotoDetailsModal = () => {
+        dispatch ({type: 'onClose'});
+      }
+      const onLoadDetail = () => {
+        dispatch ( {type: 'onLoad', payload: photoInfo})
+      }
+
+
+   const toggleFavourite = (id) => {
+     if (state.favourites.includes(id)) {
+      FAV_PHOTO_ADDED(id)
+     } else {
+
+       FAV_PHOTO_REMOVED(id)
+     }
+     
+   };
+    
+   
       
 
     return {
-        state,
+        state: state,
         toggleFavourite : toggleFavourite,
         onClosePhotoDetailsModal,
         onOpenPhotoDetailsModal,
         onLoadDetail
+
      
-
-    };
-
+            }
 }
-
+  
 
 export default useApplicationData;
 
