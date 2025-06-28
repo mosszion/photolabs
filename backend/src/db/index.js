842672ecbd980181b762a2f05195711dc3c596c0
mosszion <musdzion@gmail.com>
2025-06-27 23:38:15 -0400

    added neon database parameters

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,13 +1,9 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  host: process.env.PGHOST,
-  name: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+});;
 
 client
   .connect()
