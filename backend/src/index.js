1e8ec67a4f84a43f0db007a6df7b7365ee4fc1fa
mosszion <musdzion@gmail.com>
2025-06-30 10:04:04 -0400

    required cors library and added the frontend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,12 @@ const ENV = require("./environment");
 
 const app = require("./application")(ENV);
 const server = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://photolabs-1.onrender.com",
+  credentials: true
+}));
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
