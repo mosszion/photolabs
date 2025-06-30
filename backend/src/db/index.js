43976c1a2bb00beb78b0a183b93d3e106e744245
mosszion <musdzion@gmail.com>
2025-06-30 08:45:16 -0400

    added neon database url

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,5 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./../.env" });
+
 const pg = require("pg");
 
+console.log(process.env.DATABASE_URL);
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
@@ -7,6 +11,7 @@ const client = new pg.Client({
 
 client
   .connect()
+  .then(() => console.log("Connected to Postgres server"))
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
